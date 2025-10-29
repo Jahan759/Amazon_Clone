34498e240be90841e3f3bff053e6d72971c4c9df
Jahan759 <144882910+Jahan759@users.noreply.github.com>
2025-10-29 18:51:04 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+document.getElementById('click1').addEventListener('click',function(){
+    window.location.href = 'amazonGadget.html';
+});
+document.getElementById('click2').addEventListener('click',function(){
+  window.location.href = 'home.html';
+});
+document.getElementById('click3').addEventListener('click',function(){
+  window.location.href = 'watches.html';
+});
+document.getElementById('click4').addEventListener('click',function(){
+  window.location.href = 'fashion.html';
+});
+document.getElementById('click5').addEventListener('click',function(){
+  window.location.href = 'decor.html';
+});
+document.getElementById('click6').addEventListener('click',function(){
+  window.location.href = 'beauty.html';
+});
+document.getElementById('click7').addEventListener('click',function(){
+  window.location.href = 'family.html';
+});
+document.getElementById('click8').addEventListener('click',function(){
+  window.location.href = 'gaming.html';
+});
+
+document.getElementById('dClick').addEventListener('dblclick', function() {
+  document.getElementById('signInForm').classList.remove('hidden');
+});
